df8ed2cc3db45920ba5b6c7d5977f7b4f1ea07a3
Arbab Nadeem <arbabnadim50@gmail.com>
2026-04-22 21:15:11 +0530

    login api done for now

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,13 @@ const userModel = require("../models/userModel");
 const otpModel = require("../models/otpModel");
 const bcrypt = require("bcrypt");
 const { sendOtpEmail } = require("../utils/email");
+const jwt = require("jsonwebtoken");
 
+const generateToken = async (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "5d" });
+};
+
+// register api
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -28,7 +34,7 @@ const registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // createing user
+    // creating user
     const user = await userModel.create({
       name,
       email,
@@ -37,7 +43,7 @@ const registerUser = async (req, res) => {
       isVerified: false,
     });
 
-    // otp genarate and save to db
+    // otp generate and save to db
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
     await otpModel.create({ email, otp, action: "account_verification" });
     await sendOtpEmail(email, otp, "account_verification");
@@ -54,6 +60,46 @@ const registerUser = async (req, res) => {
       message: "internal server error!!",
     });
   }
+};
+
+// login api
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: "invalid credentials",
+      });
+    }
+
+    if (!user.isVerified && user.role === "user") {
+      const otp = Math.floor(100000 + Math.random() * 900000).toString();
+      await otpModel.deleteMany({ email, action: "account_verification" });
+      await otpModel.create({ email, otp, action: "account_verification" });
+      await sendOtpEmail(email, otp, "account_verification");
+      return res.status(400).json({
+        message: "account not verified!! please check your email for otp",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "login successfully",
+      user,
+      token: generateToken(user._id, user.role),
+    });
+  } catch (error) {}
 };
 
 module.exports = { registerUser };
