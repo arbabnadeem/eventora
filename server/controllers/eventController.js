3480a1acf19a11a395ca38f7a29c6696a625e1e5
Arbab Nadeem <arbabnadim50@gmail.com>
2026-04-28 20:53:03 +0530

    delete api done for now

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -154,7 +154,29 @@ const updateEvent = async (req, res) => {
 };
 
 // delete event
-const deleteEvent = async (req, res) => {};
+const deleteEvent = async (req, res) => {
+  try {
+    const { eventId } = req.params;
+    const event = await eventModel.findByIdAndDelete(eventId);
+    if (!event) {
+      return res.status(400).json({
+        success: false,
+        message: "event not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "event deleted successfully!!",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(400).json({
+      success: false,
+      message: "internal server error!! in delete events api",
+    });
+  }
+};
 
 module.exports = {
   getAllEvents,
