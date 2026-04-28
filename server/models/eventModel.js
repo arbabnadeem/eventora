e23d0090f5179e13d3227f035037e61321603302
Arbab Nadeem <arbabnadim50@gmail.com>
2026-04-28 22:03:42 +0530

    booking schema done for now

--- server/models/eventModel.js
+++ server/models/eventModel.js
@@ -39,7 +39,7 @@ const eventSchema = new mongoose.Schema(
     },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "userModel",
+      ref: "User",
       required: true,
     },
   },
