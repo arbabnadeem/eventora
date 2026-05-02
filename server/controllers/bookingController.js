13b62914c14db8578eb747d566336d52aa5d491d
Arbab Nadeem <arbabnadim50@gmail.com>
2026-05-02 18:38:00 +0530

    confirm booking api is done for now

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -6,6 +6,31 @@ const { sendBookingEmail, sendOtpEmail } = require("../utils/email");
 const generateOtp = () =>
   Math.floor(100000 + Math.random() * 900000).toString();
 
+// send conformation otp to confirm your booking
+const sendBookingOtp = async (req, res) => {
+  try {
+    const { user } = req.user;
+    const otp = generateOtp();
+    await otpModel.findByIdAndDelete({
+      email: user.email,
+      action: "event_booking",
+    });
+    await otpModel.create({ email: user.email, otp, action: "event_booking" });
+    await sendOtpEmail(user.email, otp, "event_booking");
+
+    return res.status(200).json({
+      success: true,
+      message: "otp send successfully for event booking",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(400).json({
+      success: false,
+      message: "internal server error!! in send Booking otp api",
+    });
+  }
+};
+
 // book your event route
 const bookEvent = async (req, res) => {
   try {
@@ -60,7 +85,7 @@ const bookEvent = async (req, res) => {
       amount: event.ticketPrice,
     });
 
-    // deleting otp
+    // deleting otp after verifying
     await bookingModel.deleteMany({
       email: req.user.email,
       action: "event_booking",
@@ -80,36 +105,71 @@ const bookEvent = async (req, res) => {
   }
 };
 
-// send conformation otp to confirm your booking
-const sendBookingOtp = async (req, res) => {
+// admin confirm your booking
+const confirmBooking = async (req, res) => {
   try {
-    const { user } = req.user;
-    const otp = generateOtp();
-    await otpModel.findByIdAndDelete({
-      email: user.email,
-      action: "event_booking",
-    });
-    await otpModel.create({ email: user.email, otp, action: "event_booking" });
-    await sendOtpEmail(user.email, otp, "event_booking");
+    const { paymentStatus } = req.body;
+
+    if (!["paid", "non_paid"].includes(paymentStatus)) {
+      return res.status(400).json({
+        success: false,
+        message: "invalid payment status!!",
+      });
+    }
+
+    const booking = await bookingModel
+      .findById(req.params.id)
+      .populate("eventId");
+    if (!booking) {
+      return res.status(400).json({
+        success: false,
+        message: "booking not found!!",
+      });
+    }
+
+    if (booking.status === "confirmed") {
+      return res.status(400).json({
+        success: false,
+        message: "Booking is already confirmed",
+      });
+    }
+
+    const event = await eventModel.findById(booking.eventId._id);
+    if (event.availableSeats <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "no seat available for this event!!",
+      });
+    }
+
+    booking.status = "confirmed";
+    if (paymentStatus) {
+      booking.paymentStatus = paymentStatus;
+    }
+
+    await booking.save();
+
+    event.availableSeats -= 1;
+    await event.save();
+
+    await sendBookingEmail(req.user.email, event.title, booking._id);
 
     return res.status(200).json({
       success: true,
-      message: "otp send successfully for event booking",
+      booking,
+      message: "Booking confirmed successfully",
     });
   } catch (error) {
     console.log(error.message);
     return res.status(400).json({
       success: false,
-      message: "internal server error!! in send Booking otp api",
+      message: "internal server error!! in confirm booking api",
     });
   }
 };
 
 // get your booked event
 const getMyBooking = async (req, res) => {};
-
-// admin confirm your booking
-const confirmBooking = async (req, res) => {};
 
 // you can cancel your booking
 const cancelBooking = async (req, res) => {};
