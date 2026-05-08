0dcc37ccb332b3ef5d465fe956e0c549ae569fc0
Arbab Nadeem <arbabnadim50@gmail.com>
2026-05-08 21:31:53 +0530

    AuthContext is done for now

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,73 @@
+import React, { createContext, use, useState } from "react";
+import api from "../utils/axios";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+    setLoading(false);
+  }, []);
+
+  const login = async (email, password) => {
+    try {
+      const { data } = await api.post("/auth/login", { email, password });
+      setUser(data);
+      localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("token", data.token);
+      return data;
+    } catch (error) {
+      console.error("login failed:", error);
+      throw error;
+    }
+  };
+
+  const register = async (name, email, password) => {
+    try {
+      const { data } = await api.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+      setUser(data);
+
+      return data;
+    } catch (error) {
+      console.error("register failed:", error);
+      throw error;
+    }
+  };
+
+  const verifyOtp = async () => {
+    try {
+      const { data } = await api.post("/auth/verify-otp");
+      setUser(data);
+      localStorage.setItem("user", JSON.stringify(data));
+      localStorage.setItem("token", data.token);
+      return data;
+    } catch (error) {
+      console.error("verify otp failed:", error);
+      throw error;
+    }
+  };
+
+  const logout = async () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, loading, login, logout, verifyOtp, register }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
