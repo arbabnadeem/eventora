ab09d5d9df0a59f3cccf2b0ee8ac74ee7397da0b
arbabnadeem <arbabnadim50@gmail.com>
2026-05-15 21:08:59 +0530

    Update API base URL in axios configuration

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://eventora-backend-mcqo.onrender.com/api",
+  baseURL: "https://eventora-backend-zpe2.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
